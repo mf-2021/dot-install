b17b53787c8b72a57f8e6f822e7551d661948b6f
mf-2021 <manamu.fujiwara@gmail.com>
2021-06-24 17:38:01 +0900

    [Finish]9

--- python/はじめてのPython/02.py
+++ python/はじめてのPython/02.py
@@ -3,12 +3,17 @@ import random
 
 answer = random.randint(1, 10)
 
+# guessの回数をカウントするためcountを用意（ここでは初期化）
+count = 0
+
 while True:
     print("Your guess? ", end="")
     guess = int(input())
+    # count = count + 1
+    count += 1
 
     if answer == guess:
-        print("Bingo!")
+        print("Bingo! It took %i guesses!" %count)
         break
     elif answer > guess:
         print("Bigger!")
