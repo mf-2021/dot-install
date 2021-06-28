396818dccbe01866c79f106f1ad9ebccb5969bd3
mf-2021 <manamu.fujiwara@gmail.com>
2021-06-28 20:30:44 +0900

    [Finish]09

--- python/python_lessons/myapp.py
+++ python/python_lessons/myapp.py
@@ -1,10 +1,12 @@
-# while
+# for
 
-i = 0
-while i < 10:
+# for 変数 in データの集合:
+#   処理
+# for i in range(0, 10):
+for i in range(10):
     if i == 5:
-        break
+        # break
+        continue
     print(i)
-    i += 1
 else:
     print("end")
