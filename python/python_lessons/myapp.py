c50e997f8a715c6148427739fd0f915714137af3
mf-2021 <manamu.fujiwara@gmail.com>
2021-06-28 20:53:41 +0900

    [Finish]12

--- python/python_lessons/myapp.py
+++ python/python_lessons/myapp.py
@@ -1,10 +1,28 @@
 # 関数
 
-def say_hi():
-    pass
-    # print("hi")
-    # return "hi"
-    # print("hello")
+# msg = "hello" # グローバル変数
 
-msg = say_hi()
+# def say_hi():
+#     msg = ("hi") # ローカル変数
+#     print(msg)
+
+# say_hi()
+# print(msg)
+
+# msg = "hello" # グローバル変数
+
+# def say_hi():
+#     print(msg)
+
+# say_hi()
+# print(msg)
+
+msg = "hello" # グローバル変数
+
+def say_hi():
+    global msg
+    msg = "hello global"
+    print(msg)
+
+say_hi()
 print(msg)
