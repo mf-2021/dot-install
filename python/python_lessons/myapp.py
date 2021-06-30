3027d5079b7b97d1830fdb3d6fe85f1189672266
mf-2021 <manamu.fujiwara@gmail.com>
2021-06-30 19:32:04 +0900

    [Finish]14

--- python/python_lessons/myapp.py
+++ python/python_lessons/myapp.py
@@ -3,16 +3,31 @@
 # user_name = "taguchi"
 # user_score = 10
 
+# class User:
+#     pass
+
+# tom = User() # インスタンス
+# tom.name = "tom"
+# tom.score = 20
+
+# bob = User()
+# bob.name = "bob"
+# bob.level = 5
+
+
 class User:
-    pass
+    """
+    コンストラクタ（Rubyでいう"def initialize"）
+    # ここで設計図（クラス）を作っているが、
+    上の例では"pass"で空のクラスを作っている
+    """
+    def __init__(self, name):
+        # インスタンス変数
+        self.name = name
 
-tom = User() # インスタンス
-tom.name = "tom"
-tom.name = 20
+tom = User("tom")
+bob = User("bob")
 
-bob = User()
-bob.name = "bob"
-bob.level = 5
 
 print(tom.name)
-print(bob.level)
+print(bob.name)
