6c11cfa8182c56d2a6a4d0b8a3b2db5d37c0702b
mf-2021 <manamu.fujiwara@gmail.com>
2021-07-16 23:36:28 +0900

    [Finish]18

--- python/python_lessons/myapp.py
+++ python/python_lessons/myapp.py
@@ -36,21 +36,24 @@
 # print(tom._name)
 # tom.say_hi()
 
+# クラスの継承
+# User -> AdminUser
+
 class User:
-    # クラス変数
-    count = 0
     def __init__(self, name):
-        User.count += 1
-        self.__name = name
+        self.name = name
     # インスタンスメソッド
     def say_hi(self):
-        print("hi {0}".format(self.__name))
-    # クラスメソッド
-    @classmethod
-    def show_info(cls):
-        print("{0} instances".format(cls.count))
+        print("hi {0}".format(self.name))
 
-tom = User("tom")
-# print(tom.__name)
-print(tom._User__name)
-tom.say_hi()
+class AdminUser(user):
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+    def say_hello(self):
+        print("hello {0} ({1})".format(self.name, self.age))
+
+bob = AdminUser("bob", 23)
+print(bob.name)
+bob.say_hi()
+bob.say_hello()
