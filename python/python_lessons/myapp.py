59ce7fdbbb04e328555bdd4b59614f972d173e78
mf-2021 <manamu.fujiwara@gmail.com>
2021-07-20 09:27:44 +0900

    [Finish]19

--- python/python_lessons/myapp.py
+++ python/python_lessons/myapp.py
@@ -1,59 +1,22 @@
-# クラス
-# - アクセス制限
+# クラスの多重継承
+# A, B -> C
 
-# user_name = "taguchi"
-# user_score = 10
-
-# class User:
-#     pass
-
-# tom = User() # インスタンス
-# tom.name = "tom"
-# tom.score = 20
-
-# bob = User()
-# bob.name = "bob"
-# bob.level = 5
-# python
-
-# クラス
-
-# class User:
-#     # クラス変数
-#     count = 0
-#     def __init__(self, name):
-#         User.count += 1
-#         self._name = name
-#     # インスタンスメソッド
-#     def say_hi(self):
-#         print("hi {0}".format(self._name))
-#     # クラスメソッド
-#     @classmethod
-#     def show_info(cls):
-#         print("{0} instances".format(cls.count))
-
-# tom = User("tom")
-# print(tom._name)
-# tom.say_hi()
-
-# クラスの継承
-# User -> AdminUser
-
-class User:
-    def __init__(self, name):
-        self.name = name
-    # インスタンスメソッド
+class A:
+    def say_a(self):
+        print("A!")
     def say_hi(self):
-        print("hi {0}".format(self.name))
+        print("hi! from A!")
+class B:
+    def say_b(self):
+        print("B!")
+    def say_hi(self):
+        print("hi! from B!")
 
-class AdminUser(user):
-    def __init__(self, name, age):
-        super().__init__(name)
-        self.age = age
-    def say_hello(self):
-        print("hello {0} ({1})".format(self.name, self.age))
+# class C(A, B):
+class C(B, A):
+    pass
 
-bob = AdminUser("bob", 23)
-print(bob.name)
-bob.say_hi()
-bob.say_hello()
+c = C()
+# c.say_a()
+# c.say_b()
+c.say_hi()
