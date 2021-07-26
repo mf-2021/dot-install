90807a232d016f6706d7592e3612f191cf993703
mf-2021 <manamu.fujiwara@gmail.com>
2021-07-26 18:41:21 +0900

    [Finish]22

--- python/python_lessons/myapp.py
+++ python/python_lessons/myapp.py
@@ -2,13 +2,36 @@
 
 # import user
 # from user import AdminUser
-from user import AdminUser, User
+# from user import AdminUser, User
 
-# bob = user.AdminUser("bob", 23)
-bob = AdminUser("bob", 23)
+# # bob = user.AdminUser("bob", 23)
+# bob = AdminUser("bob", 23)
 
-tom = User("bob", 23)
+# tom = User("bob", 23)
 
-print(bob.name)
-bob.say_hi()
-bob.say_hello()
+# print(bob.name)
+# bob.say_hi()
+# bob.say_hello()
+
+# 例外処理
+
+class MyException(Exception):
+    pass
+
+def div(a, b):
+    try:
+        if (b < 0):
+            raise MyException("not minus")
+        print(a / b)
+    except ZeroDivisionError:
+        print("not by zero!")
+    except MyException as e:
+        print(e)
+    else:
+        print("no exception!")
+    finally:
+        print("-- end --")
+
+div(10, -3)
+# div(10, 3)
+# div(10, 0)
