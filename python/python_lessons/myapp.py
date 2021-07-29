7267a4b2fd0e4263094daf1de79ba0ce542cda4c
mf-2021 <manamu.fujiwara@gmail.com>
2021-07-29 09:34:47 +0900

    [Finish]24

--- python/python_lessons/myapp.py
+++ python/python_lessons/myapp.py
@@ -4,15 +4,25 @@
 
 # リスト型
 
-scores = [40, 50]
-# print(scores[0]) # 40
-# scores[0] = 45
-# print(len(scores)) # 2
-# scores.append(100)
-# print(scores)
+# scores = [40, 50]
+# # print(scores[0]) # 40
+# # scores[0] = 45
+# # print(len(scores)) # 2
+# # scores.append(100)
+# # print(scores)
 
-# for score in scores:
-#     print(score)
+# # for score in scores:
+# #     print(score)
 
-for i, score in enumerate(scores):
-    print("{0}: {1}".format(i, score))
+# for i, score in enumerate(scores):
+#     print("{0}: {1}".format(i, score))
+
+
+# タプル
+
+# items = (50, "apple", 32.5)
+# print(items[1])
+# items[1] = "pen"
+
+print(list((1, 3, 5)))
+print(tuple([1, 3, 5]))
