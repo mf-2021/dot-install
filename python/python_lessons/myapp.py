da39229a4cc1e381785d953ced33813237e4a2f6
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-10 23:58:15 +0900

    [Finish]30

--- python/python_lessons/myapp.py
+++ python/python_lessons/myapp.py
@@ -94,4 +94,11 @@
 # print(list(map(triple, [1, 2, 3])))
 
 # lambda 引数：　処理
-print(list(map(lambda n: n * 3, [1, 2, 3])))
+# print(list(map(lambda n: n * 3, [1, 2, 3])))
+
+# filter(関数, イテレータ)
+
+# def is_even(n):
+#     return n % 2 == 0
+
+print(list(filter(lambda n: n % 2 == 0, range(10))))
