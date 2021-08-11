181ea251b9f71d4f1cdecc3b72fefb8f93a15c8e
mf-2021 <manamu.fujiwara@gmail.com>
2021-08-11 11:05:32 +0900

    [Finish]31

--- python/python_lessons/myapp.py
+++ python/python_lessons/myapp.py
@@ -101,4 +101,12 @@
 # def is_even(n):
 #     return n % 2 == 0
 
-print(list(filter(lambda n: n % 2 == 0, range(10))))
+# print(list(filter(lambda n: n % 2 == 0, range(10))))
+
+# 内包表記
+
+# 0 - 9
+# print([i for i in range(10)])
+# print([i * 3 for i in range(10) if i % 2 == 0])
+print(i * 3 for i in range(10) if i % 2 == 0)
+print({i * 3 for i in range(10) if i % 2 == 0})
